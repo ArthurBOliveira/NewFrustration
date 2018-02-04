b4bb831beca878582b8091b86b6e5631d647ae9e
ArthurBOliveira <arthurboliveira93@gmail.com>
2018-02-04 17:49:05 -0200

    Initial Commit

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public int status;
+
+    private SpriteRenderer rend;
+    private Rigidbody2D rg2d;
+
+    private bool isKilling;
+
+    private void Awake()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        rg2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        isKilling = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isKilling && collision.collider.tag == "player")
+            collision.collider.GetComponent<Player>().Die();
+    }
+
+    private void Update()
+    {
+        switch (status)
+        {
+            case 0:
+                turnNormal();
+                break;
+
+            case 1:
+                turnKilling();
+                break;
+        }
+    }
+
+    private void turnNormal()
+    {
+        rend.color = Color.white;
+        rg2d.sharedMaterial = new PhysicsMaterial2D() {  };
+    }
+
+    private void turnKilling()
+    {
+        rend.color = Color.red;
+        isKilling = true;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+
+    public bool grounded;
+
+    private Rigidbody2D rg2d;
+
+    private void Awake()
+    {
+        rg2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //moving
+        float h = Input.GetAxis("Horizontal");
+
+        rg2d.AddForce(Vector2.right * h * speed);
+
+        //jumping
+        if (grounded && Input.GetKeyDown(KeyCode.Space)) rg2d.AddForce(Vector2.up * jump);
+    }
+
+    private void OnCollisionStay2D(Collision2D collider)
+    {
+        GroundCheck();
+    }
+
+    public void Die()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnCollisionExit2D(Collision2D collider)
+    {
+        grounded = false;
+    }
+
+    private void GroundCheck()
+    {
+        RaycastHit2D[] hits;
+
+        //We raycast down 1 pixel from this position to check for a collider
+        Vector2 positionToCheck = transform.position;
+        hits = Physics2D.RaycastAll(positionToCheck, new Vector2(0, -1), 0.01f);
+
+        //if a collider was hit, we are grounded
+        if (hits.Length > 0)
+        {
+            grounded = true;
+        }
+    }
+}
