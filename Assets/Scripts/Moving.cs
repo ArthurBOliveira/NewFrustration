1952140a8ed99be3deb92663f5834fab499ac1a7
ArthurBOliveira <arthurboliveira93@gmail.com>
2018-02-09 00:44:46 -0200

    Finished 3rd level working on 4th

---
+++ Assets/Scripts/Moving.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    public bool left;
+    public float speed;
+
+    public float ending;
+
+    private Rigidbody2D rg2d;
+
+    private void Awake()
+    {
+        rg2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        if (left)
+            rg2d.velocity = Vector2.left * speed;
+        else
+            rg2d.velocity = Vector2.right * speed;
+    }
+
+    private void Update()
+    {
+        if (left && transform.position.x <= ending)
+            Destroy(gameObject);
+        else if (!left && transform.position.x >= ending)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlatformSpawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+    public bool left;
+    public float speed;
+    public float spawnTime;
+    public float despawnTime;
+
+    public GameObject platform;
+    public GameObject end;
+
+    public bool isActive;
+
+    private void Start()
+    {
+        isActive = false;
+        StartCoroutine(BeginSpawn());
+    }
+
+    private void Update()
+    {
+        isActive = end.GetComponent<LevelController>().isActive;
+    }
+
+    private IEnumerator BeginSpawn()
+    {
+        while (true)
+        {
+            if (isActive)
+            {
+                Spawn();
+            }
+            yield return new WaitForSeconds(spawnTime);
+        }
+    }
+
+    private void Spawn()
+    {
+        Debug.Log("Spawnou!");
+        GameObject plat = Instantiate(platform, transform.position, Quaternion.identity);
+
+        plat.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+        plat.transform.localScale = new Vector3(0.5f, plat.transform.localScale.y);
+
+        if (left)
+            plat.GetComponent<Rigidbody2D>().velocity = Vector2.left * speed;
+        else
+            plat.GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+
+        Destroy(plat, despawnTime);
+    }
+}
